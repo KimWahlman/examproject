3353873558ce9ee39d26f3db8960be20fc0e5391
Kim Wahlman <kim.wahlman@gmail.com>
2016-03-02 16:42:23 +0100

    Added some code
    Added some code before heading home, no big stuff

--- src/CellularAutomata/CellularAutomata.h
+++ src/CellularAutomata/CellularAutomata.h
@@ -7,8 +7,17 @@
 typedef struct 
 {
 	// TODO: Fixa bättre dokumentation
-	int WallProx1,	// Hur många grannar är väggar?
-		WallProx2;
+	int WallNeighbours1,	// Hur många grannar är väggar?
+		WallNeighbours2;
+
+	int RepeatSimXTurns;	// How many generations will we generated?
+} GenerationParameters;
+
+class CellularAutomata
+{
+	double	FillProbability;
+	int		SizeX, SizeY;
+public:
 };
 
 #endif
