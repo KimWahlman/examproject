f81d8ca98ba774859c3f4a02e19f31224532fda7
Kim Wahlman <kim.wahlman@gmail.com>
2016-03-26 01:04:44 +0100

    Removed sfml-window drawing, now saves a image of the cave (10k x 10k will not result in an image).

--- src/SFMLStuff.cpp
+++ src/SFMLStuff.cpp
@@ -1,21 +1,9 @@
 #include "SFMLStuff.h"
 #include "DLA\DLA.h"
 #include "CellularAutomata\CellularAutomata.h"
-#include "Misc\misc.h"
-#include <iostream>
 
-#define SIZEX 10.0f
-#define SIZEY 10.0f
-
-void MessyClass::Init(int sizeX, int sizeY)
+void MessyClass::Init()
 {
-	mWindow.create(sf::VideoMode(sizeX, sizeY), "test");
-	mWindow.setTitle("Exjobb - DLA");
-	mView.setSize((float)sizeX, float(sizeY));
-	mView.zoom(20.0f);
-	mView.setViewport(sf::FloatRect(0.0f, 0.0f, 1.0f, 1.0f));
-	//mView.setCenter(DLA::GetInstance().GetSizeX(), DLA::GetInstance().GetSizeY());
-	mView.setCenter(sizeX / 2, sizeY / 2);
 	drawFluff = new char*[DLA::GetInstance().GetSizeY()];
 	for (int y = 0; y < DLA::GetInstance().GetSizeY(); y++)
 		drawFluff[y] = new char[DLA::GetInstance().GetSizeX()];
@@ -23,106 +11,43 @@ void MessyClass::Init(int sizeX, int sizeY)
 	for (int y = 0; y < DLA::GetInstance().GetSizeY(); y++)
 		for (int x = 0; x < DLA::GetInstance().GetSizeX(); x++)
 			drawFluff[y][x] = '#';
-
-	mWhite.setSize(sf::Vector2f(SIZEX, SIZEY));
-	mWhite.setFillColor(sf::Color::White);
-	mCurrX = 0.0f;
-	mCurrY = 0.0f;
-	mCaveSizeX = DLA::GetInstance().GetSizeX();
-	mCaveSizeY = DLA::GetInstance().GetSizeX();
-	mCurrZoom = 1.0f;
-}
-
-void MessyClass::DrawWindow()
-{
-	mWindow.clear();
-	mWindow.setView(mView);
-	DrawMap();
-	mWindow.display();
 }
 
 void MessyClass::Run()
 {
 	FetchData();
-	while (mWindow.isOpen())
-	{
-		sf::Event _event;
-		while (mWindow.pollEvent(_event))
-		{
-			if (_event.type == sf::Event::Closed || sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) {
-				mWindow.close();
-				exit(0);
-			}
-		}
-		Keyboard();
-		DrawWindow();
-	}
-}
-
-void MessyClass::Keyboard()
-{
-	float tempX = (mCaveSizeX / 2);
-	float tempY = (mCaveSizeX / 2);//(mCaveSizeY / (mCaveSizeY / 2));
-	if (tempX < 1.0f) tempX = 10.0f;
-	if (tempY < 1.0f) tempX = 10.0f;
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
-		mView.move(tempX, 0);
-	}
-	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
-		mView.move(-tempX, 0);
-	}
-
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
-		mView.move(0, tempY);
-	}
-	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
-		mView.move(0, -tempY);
-	}
-	
-	//if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
-	//	std::cout << mCurrZoom << "\n";
-	//	mWindow.setKeyRepeatEnabled(false);
-	//	mCurrZoom += 1.0f;
-	//	mView.zoom(mCurrZoom);
-	//}
-	//if (sf::Keyboard::isKeyPressed(sf::Keyboard::B)) {
-	//	std::cout << mCurrZoom << "\n";
-	//	mWindow.setKeyRepeatEnabled(false);
-	//	mCurrZoom -= 1.0f;
-	//	mView.zoom(mCurrZoom);
-	//}
+	SaveImage();
 }
 
 void MessyClass::FetchData()
 {
 	drawFluff = DLA::GetInstance().GetCave();
-	mCaveSizeX = DLA::GetInstance().GetSizeX();
-	mCaveSizeY = DLA::GetInstance().GetSizeY();
-	//mWindow.setSize(sf::Vector2u(mCaveSizeX, mCaveSizeY));
-	//for (int y = 0; y < DLA::GetInstance().GetSizeY(); y++) {
-	//	for (int x = 0; x < DLA::GetInstance().GetSizeX(); x++)
-	//		std::cout << drawFluff[y][x];
-	//	std::cout << "\n";
-	//}
 }
 
-void MessyClass::DrawMap()
+void MessyClass::SaveImage()
 {
-	for (int y = 0; y < DLA::GetInstance().GetSizeY(); y++)
-	{
-		for (int x = 0; x < DLA::GetInstance().GetSizeX(); x++)
-		{
-			
-			if (drawFluff[y][x] == '.') 
-			{
-				mWhite.setPosition(mCurrX, mCurrY);
-				mWindow.draw(mWhite);
+	sf::Uint8* temp;
+	temp = new sf::Uint8[DLA::GetInstance().GetSizeY() * DLA::GetInstance().GetSizeX() * 4];
+	int cx = DLA::GetInstance().GetSizeX(),
+		cy = DLA::GetInstance().GetSizeY();
+
+	for (int y = 0; y < cy; y++) {
+		for (int x = 0; x < cx; x++) {
+			if (drawFluff[x][y] == '.') {
+				temp[(y + x * cy) * 4 + 0] = 255;
+				temp[(y + x * cy) * 4 + 1] = 255;
+				temp[(y + x * cy) * 4 + 2] = 255;
+				temp[(y + x * cy) * 4 + 3] = 255;
 			}
-			mCurrX += SIZEX;
+			if (drawFluff[x][y] == '#') {
+				temp[(y + x * cy) * 4 + 0] = 0;
+				temp[(y + x * cy) * 4 + 1] = 0;
+				temp[(y + x * cy) * 4 + 2] = 0;
+				temp[(y + x * cy) * 4 + 3] = 255;
+			}
 		}
-		mCurrX = 0.0f;
-		mCurrY += SIZEY;
 	}
-	mCurrX = 0.0f;
-	mCurrY = 0.0f;
+
+	img.create(DLA::GetInstance().GetSizeX(), DLA::GetInstance().GetSizeY(), temp);
+	img.saveToFile("img/cave_" + std::to_string(DLA::GetInstance().GetCavesGenerated()) + ".png");
 }
