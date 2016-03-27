9f7eebb5442cec3507c9b8c5b9a4c850bc1fb163
Kim Wahlman <kim.wahlman@gmail.com>
2016-03-27 18:54:45 +0200

    Cleaned up SFMLStuff a little.

--- src/SFMLStuff.cpp
+++ src/SFMLStuff.cpp
@@ -22,28 +22,18 @@ void MessyClass::FetchData(char** cave)
 
 void MessyClass::SaveImage(int num, char** cave)
 {
-	//drawFluff = new char*[caveX];
-	//for (int y = 0; y < caveX; y++)
-	//	drawFluff[y] = new char[caveX];
-
-	//for (int y = 0; y < caveX; y++)
-	//	for (int x = 0; x < caveX; x++)
-	//		drawFluff[y][x] = '#';
-
-	//drawFluff = cave;
-
  	sf::Uint8* temp;
 	temp = new sf::Uint8[caveY * caveX * 4];
 	memset(temp, 255, caveY * caveX * 4);
 
 	for (int y = 0; y < caveY; y++) {
 		for (int x = 0; x < caveX; x++) {
-			if (cave[x][y] == '.') {
-				//temp[(y + x * caveY) * 4 + 0] = 255;
-				//temp[(y + x * caveY) * 4 + 1] = 255;
-				//temp[(y + x * caveY) * 4 + 2] = 255;
-				//temp[(y + x * caveY) * 4 + 3] = 255;
-			}
+			//if (cave[x][y] == '.') {
+			//	//temp[(y + x * caveY) * 4 + 0] = 255;
+			//	//temp[(y + x * caveY) * 4 + 1] = 255;
+			//	//temp[(y + x * caveY) * 4 + 2] = 255;
+			//	//temp[(y + x * caveY) * 4 + 3] = 255;
+			//}
 			if (cave[x][y] == '#') {
 				temp[(y + x * caveY) * 4 + 0] = 0;
 				temp[(y + x * caveY) * 4 + 1] = 0;
@@ -62,7 +52,4 @@ void MessyClass::SaveImage(int num, char** cave)
 	img.create(caveX, caveY, temp);
 	img.saveToFile("img/cave_" + std::to_string(num) + ".png");
 	delete[] temp;
-	//for (int i = 0; i < caveY; i++)
-	//	delete drawFluff[i];
-	//delete drawFluff;
 }
